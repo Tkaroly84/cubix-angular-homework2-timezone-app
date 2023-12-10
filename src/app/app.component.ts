ece6c3dd9cb2b6fa865184240b854e65a613b280
Tóth Károly <toth.karolycar84@gmail.com>
2023-12-10 09:31:42 +0100

    version 1.0-basic app succes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { TimeComponent } from './time/time.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet,TimeComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
